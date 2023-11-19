f8ca8b4df51e2a0211fc67a4c9e14522f3869e90
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2023-11-19 23:03:27 +0100

    enable expanding navbar add tutorial documentation nav links

--- src/lib/layout/Navbar.svelte
+++ src/lib/layout/Navbar.svelte
@@ -4,7 +4,7 @@
 	import ThreeDotsVertical from '$lib/icons/ThreeDotsVertical.svelte';
 	import ThreeDotsHorizontal from '$lib/icons/ThreeDotsHorizontal.svelte';
 	import { onMount } from 'svelte';
-	import { showOffset, isIndexPage } from '$lib/stores';
+	import { showOffset, isIndexPage, isScreenLessThan992 } from '$lib/stores';
 
 	let theme: 'light' | 'dark';
 
@@ -20,6 +20,16 @@
 		}
 	}
 
+	let isNavbarExpanded = false;
+
+	function toggleNavbar() {
+		if (!isScreenLessThan992) return;
+
+		isNavbarExpanded = !isNavbarExpanded;
+
+		// console.log({ isNavbarExpanded });
+	}
+
 	onMount(() => {
 		theme = document.documentElement.getAttribute('data-theme') as 'light' | 'dark';
 	});
@@ -28,56 +38,152 @@
 <header>
 	<div class="container">
 		<div class="header-content">
-			<!-- MENU TOGGLE -->
-			<span
-				style={$isIndexPage ? 'display: none' : 'display: block'}
-				on:click={() => showOffset.set(!$showOffset)}
-				class="icon"
-				id="menu-toggle"
-			>
-				{#if $showOffset}
-					<ThreeDotsHorizontal />
-				{:else}
-					<ThreeDotsVertical />
-				{/if}
-			</span>
+			<div class="left-section" style={$isIndexPage ? 'display: none' : 'display: inline'}>
+				<!-- MENU TOGGLE -->
+				<span on:click={() => showOffset.set(!$showOffset)} class="icon" id="menu-toggle">
+					{#if $showOffset}
+						<ThreeDotsHorizontal />
+					{:else}
+						<ThreeDotsVertical />
+					{/if}
+				</span>
+			</div>
 
-			<!-- NAV BRAND -->
-			<a href="/" id="nav-brand"><strong>kodujemywbiurze.pl</strong></a>
+			<div class="center-section">
+				<!-- NAV BRAND -->
+				<a href="/" class="nav-link" id="nav-brand"><strong>kodujemywbiurze.pl</strong></a>
+			</div>
 
-			<!-- THEME TOGGLE -->
-			<span on:click={switchMode} class="icon" id="theme-toggle">
-				{#if theme === 'dark'}
-					<SunIcon />
-				{:else}
-					<MoonIcon />
+			<div class="right-section">
+				{#if !$isScreenLessThan992}
+					<a href="/wprowadzenie-co-stworzymy-i-czego-sie-nauczymy" class="nav-link">tutorial</a>
+					<a href="/dokumentacja/javascript/wprowadzenie" class="nav-link">dokumentacja</a>
 				{/if}
-			</span>
+
+				<!-- THEME TOGGLE -->
+				<span on:click={switchMode} class="icon" id="theme-toggle">
+					{#if theme === 'dark'}
+						<SunIcon />
+					{:else}
+						<MoonIcon />
+					{/if}
+				</span>
+
+				<!-- NAVBAR TOGGLE -->
+				<span
+					style={!$isScreenLessThan992 ? 'display: none' : 'display: inline; padding-left: 0.5em'}
+					on:click={toggleNavbar}
+					class="icon"
+					id="navbar-toggle"
+				>
+					{#if isNavbarExpanded}
+						<ThreeDotsHorizontal />
+					{:else}
+						<ThreeDotsVertical />
+					{/if}
+				</span>
+			</div>
 		</div>
+
+		{#if isScreenLessThan992 && isNavbarExpanded}
+			<hr />
+			<div class="expanded-nav-section">
+				<a
+					href="/wprowadzenie-co-stworzymy-i-czego-sie-nauczymy"
+					class="nav-link"
+					on:click={toggleNavbar}>tutorial</a
+				>
+				<a href="/dokumentacja/javascript/wprowadzenie" class="nav-link" on:click={toggleNavbar}
+					>dokumentacja</a
+				>
+			</div>
+		{/if}
 	</div>
 </header>
 
 <style>
+	header {
+		background-color: var(--header-background-color);
+		line-height: 2em;
+		min-height: 40px;
+		position: fixed;
+		z-index: 99;
+		width: 100%;
+	}
+
+	header .header-content {
+		background-color: var(--header-background-color);
+	}
+
 	.header-content {
 		display: flex;
-		justify-content: space-between;
 		color: white;
+		align-items: center;
 	}
 
 	#nav-brand {
 		text-decoration: none;
 		font-size: large;
-		padding-top: 3px;
 	}
 
-	.header-content a,
+	.header-content,
+	.nav-link,
 	.icon {
 		color: white;
 	}
 
-	.header-content a:hover,
+	.nav-link {
+		text-decoration: none;
+		padding: 0 0.5em;
+	}
+
+	.header-content,
+	.nav-link:hover,
 	.icon:hover {
 		color: rgba(255, 255, 255, 0.7);
+	}
+
+	.left-section,
+	.right-section {
+		flex: 1;
+		display: inline-block;
+		vertical-align: middle;
+	}
+
+	.right-section {
+		text-align: end;
+	}
+
+	.center-section {
+		/* If you want the center section to stay centered regardless of right content growth */
+		flex: 0 1 auto; /* This allows the center section to shrink and grow as needed, but not shrink smaller than its content */
+		margin: 0 auto; /* This centers the center section */
+	}
+
+	/* Style for icons, logos, or other spans */
+	span {
+		display: inline-block;
+		vertical-align: middle;
+	}
+
+	/* Style for links in the right section */
+	.right-section a {
+		display: inline-block;
+		vertical-align: middle;
+		/* Add other styles as needed */
+	}
+
+	.expanded-nav-section {
+		display: flex;
+		flex-direction: column;
+
+		/* & a {
+			color: white;
+		}
+
+		& a:hover {
+			color: rgba(255, 255, 255, 0.7);
+		} */
 	}
 
 	/* Styles for screens with a minimum width of 992px (typical laptop) */
