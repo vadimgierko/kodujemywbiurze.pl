04bdf5da34a520f8090f6f051bb0b176e605d541
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2023-12-07 23:59:38 +0100

    add basic HTML course intro and articles

---
+++ src/routes/kursy/html/[slug]/+page.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import PrevNextArticle from '$lib/components/PrevNextArticle.svelte';
+	import { marked } from 'marked';
+	import { isIndexPage } from '$lib/stores';
+	import { onMount, afterUpdate } from 'svelte';
+	import type { Article } from '$lib/types/index.js';
+
+	//========================== CODE HIGHLIGHTING =======================//
+	// import hljs from 'highlight.js';
+	import hljs from 'highlight.js/lib/core';
+	import xml from 'highlight.js/lib/languages/xml';
+
+	hljs.registerLanguage('javascript', xml);
+
+	// Function to dynamically load dark/light CSS based on the theme
+	function loadHighlightTheme() {
+		const theme = document.documentElement.getAttribute('data-theme');
+		const link = document.createElement('link');
+		link.rel = 'stylesheet';
+
+		if (theme === 'dark') {
+			link.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/vs2015.css';
+		} else {
+			link.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/vs.css';
+		}
+
+		// Remove any existing highlight.js styles
+		const existingStyles = document.querySelectorAll(
+			'link[href*="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/"]'
+		);
+		existingStyles.forEach((style) => style.remove());
+
+		document.head.appendChild(link);
+	}
+	//========================== CODE HIGHLIGHTING: END =======================//
+
+	export let data;
+
+	const { articles } = data;
+
+	$: article = articles.find((a) => a.slug === $page.params.slug);
+	$: articleIndex = articles.findIndex((a) => a.slug === $page.params.slug);
+	$: prevArticleSlug = articleIndex > 0 ? articles[articleIndex - 1].slug : null;
+	$: nextArticleSlug =
+		articleIndex !== articles.length - 1 ? articles[articleIndex + 1].slug : null;
+
+	onMount(() => {
+		isIndexPage.set(false);
+
+		//========================== CODE HIGHLIGHTING =======================//
+		loadHighlightTheme();
+
+		// Observer to watch for changes in data-theme attribute
+		const observer = new MutationObserver((mutationsList) => {
+			for (let mutation of mutationsList) {
+				if (mutation.type === 'attributes' && mutation.attributeName === 'data-theme') {
+					loadHighlightTheme();
+				}
+			}
+		});
+
+		// Start observing changes in the data-theme attribute
+		observer.observe(document.documentElement, { attributes: true });
+
+		//========================== CODE HIGHLIGHTING: END =======================//
+	});
+
+	// Initialize highlighting:
+	afterUpdate(() => {
+		document.querySelectorAll('code').forEach((block) => {
+			hljs.highlightElement(block);
+		});
+	});
+</script>
+
+<svelte:head>
+	<title>Kodujemy w biurze | {article?.title}</title>
+	<meta property="og:title" content={`Kodujemy w biurze | ${article?.title}`} />
+
+	<meta name="description" content={`${article?.title}`} />
+	<meta property="og:description" content={`${article?.title}`} />
+
+	<meta
+		property="og:image"
+		content="/content/kursy/javascript/kurs-podstawy-javascript-pierwsza-aplikacja-w-konsoli-screenshot.jpg"
+	/>
+
+	<meta
+		property="og:url"
+		content={`https://www.kodujemywbiurze.pl/kursy/javascript/${$page.params.slug}`}
+	/>
+	<meta property="og:type" content="article" />
+</svelte:head>
+
+{#if article}
+	<PrevNextArticle
+		prevSlug={prevArticleSlug ? `kursy/html/${prevArticleSlug}` : null}
+		nextSlug={nextArticleSlug ? `kursy/html/${nextArticleSlug}` : null}
+	/>
+	<article>
+		{@html marked(article.content)}
+	</article>
+	<PrevNextArticle
+		prevSlug={prevArticleSlug ? `kursy/html/${prevArticleSlug}` : null}
+		nextSlug={nextArticleSlug ? `kursy/html/${nextArticleSlug}` : null}
+	/>
+{:else}
+	<p style="text-align: center; color: red">Niestety nie ma takiego artykułu...</p>
+{/if}
